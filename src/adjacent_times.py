5fe3709bc7d5b9cf8389fadb2d884e021f77ef89
Kavi Gupta <kavi@globalmbox.com>
2017-01-02 14:22:56 -0800

    Basic adjacent time module

---
+++ src/adjacent_times.py
@@ -0,0 +1,18 @@
+
+from evaluations import GradedMidterm
+
+def find_adjacent_times(graded_exams):
+    """
+    Inputs
+        graded_exams: a dictionary: Run_Count -> Graded_Exam
+    Output
+        a list from question number -> [(Evaluation A , Evaluation B)] where A and B are adjacent
+    """
+    exam_list = [graded_exams[x] for x in range(len(graded_exams))]
+    for question in range(len(exam_list[0].evals)):
+        adjacents = []
+        for prev, curr in zip(exam_list, exam_list[1:]):
+            prev_q, curr_q = prev.evals[question], curr.evals[question]
+            if prev_q.grader == curr_q.grader:
+                adjacents.append((GradedMidterm(prev.name, prev.email, prev_q), GradedMidterm(curr.name, curr.email, curr_q)))
+        yield adjacents
