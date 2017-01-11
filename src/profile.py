592990ca256394065a8edcb9d2dce5c1d3213c4f
Kavi Gupta <kavi@globalmbox.com>
2017-01-10 16:04:04 -0800

    Added profiler

---
+++ src/profile.py
@@ -0,0 +1,25 @@
+
+from models import plausible_parameters, RandomSeatingModel, binary_cheater, score_diff_summary
+from graphics import TerminalProgressBar
+from evaluations import proc_evaluations
+from seating_chart import SeatingChart
+from constants import DATA_DIR
+
+from sys import argv
+
+def main(arg):
+    evals = proc_evaluations('%s/real-data/Midterm_1_evaluations.zip' % DATA_DIR)
+    seats = SeatingChart('%s/real-data/mt1_seats.csv' % DATA_DIR)
+    if arg == "--plausible-params":
+        profile_plausible_params(evals, seats)
+    else:
+        raise RuntimeError("Argument %s not recognized" % arg)
+
+
+def profile_plausible_params(evals, seats):
+    list(plausible_parameters(evals, seats,
+                                  binary_cheater(RandomSeatingModel, ()),
+                                  score_diff_summary, 20, 1, TerminalProgressBar))
+
+if __name__ == '__main__':
+    main(argv[1])
